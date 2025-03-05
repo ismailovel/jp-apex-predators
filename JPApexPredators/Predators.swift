02e0fa44fa3f6bee531a268f51b96fb22a09377a
Elchin Ismailov <ismailowwork@gmail.com>
2025-03-05 14:06:43 +0400

    feat: implement JSON decoding for ApexPredators

---
+++ JPApexPredators/Predators.swift
@@ -0,0 +1,29 @@
+//
+//  Predators.swift
+//  JPApexPredators
+//
+//  Created by Ismailov on 5.03.2025.
+//
+
+import Foundation
+
+class Predators {
+    var apexPredators: [ApexPredator] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            } catch {
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+}
