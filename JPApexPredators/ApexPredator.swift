782de9545d4e1d54e9bf4c98638887880ff8fda9
Elchin Ismailov <ismailowwork@gmail.com>
2025-03-06 16:58:55 +0400

    feat: predators list, search, sort

--- JPApexPredators/ApexPredator.swift
+++ JPApexPredators/ApexPredator.swift
@@ -4,20 +4,62 @@
 //
 //  Created by Ismailov on 5.03.2025.
 //
+import SwiftUI
 
-struct ApexPredator: Decodable {
+struct ApexPredator: Decodable, Identifiable {
     let id: Int
     let name: String
-    let type: String
+    let type: APType
     let latitude: Double
     let longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
     let link: String
     
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
     struct MovieScene: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+}
+
+enum APType: String, Decodable, CaseIterable, Identifiable {
+    case all
+    case land
+    case air
+    case sea
+    
+    var id: APType {
+        self
+    }
+    
+    var background: Color {
+        switch self {
+        case .all:
+                .black
+        case .land:
+                .brown
+        case .air:
+                .teal
+        case .sea:
+                .blue
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .all:
+            "square.stack.3d.up.fill"
+        case .land:
+            "leaf.fill"
+        case .air:
+            "wind"
+        case .sea:
+            "drop.fill"
+        }
     }
 }
