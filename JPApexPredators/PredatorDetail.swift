b640aea1b9b39015a3475f0091687664c9888835
Elchin Ismailov <ismailowwork@gmail.com>
2025-03-08 21:46:25 +0400

    feat: create and populate PredatorDetail screen

---
+++ JPApexPredators/PredatorDetail.swift
@@ -0,0 +1,77 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators
+//
+//  Created by Ismailov on 7.03.2025.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    let predator: ApexPredator
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                ZStack(alignment: .bottomTrailing) {
+                    // background image
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    // dino image
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width / 1.5, height: geo.size.height / 3.7)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20)
+                }
+                
+                VStack(alignment: .leading) {
+                    // dino name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    // current location
+                    
+                    // appears in
+                    Text("Appears In:")
+                        .font(.title3)
+                    
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("•" + movie)
+                            .font(.subheadline)
+                    }
+                    // movie moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    
+                    ForEach(predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    // link to webpage
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    PredatorDetail(predator: Predators().apexPredators[2])
+    //        .preferredColorScheme(.dark)
+}
