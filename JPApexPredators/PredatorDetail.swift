9a7907907c338a5694de7aa411bce40e077bd512
Elchin Ismailov <ismailowwork@gmail.com>
2025-03-09 00:22:11 +0400

    feat: add background image gradient and enforce portrait orientation

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -18,6 +18,14 @@ struct PredatorDetail: View {
                     Image(predator.type.rawValue)
                         .resizable()
                         .scaledToFit()
+                        .overlay {
+                            LinearGradient(stops: [
+                                Gradient.Stop(color: .clear,
+                                              location: 0.8),
+                                Gradient.Stop(color: .black,
+                                              location: 1)
+                            ], startPoint: .top, endPoint: .bottom)
+                        }
                     // dino image
                     Image(predator.image)
                         .resizable()
@@ -73,5 +81,5 @@ struct PredatorDetail: View {
 
 #Preview {
     PredatorDetail(predator: Predators().apexPredators[2])
-    //        .preferredColorScheme(.dark)
+            .preferredColorScheme(.dark)
 }
