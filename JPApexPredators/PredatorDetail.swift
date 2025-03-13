547ba3c484b038222e5c23724d3341965c103083
Elchin Ismailov <ismailowwork@gmail.com>
2025-03-13 23:02:52 +0400

    add zoom transition and iMessage Stickers

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -13,6 +13,8 @@ struct PredatorDetail: View {
     
     @State var position: MapCameraPosition
     
+    @Namespace var namespace
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -45,9 +47,13 @@ struct PredatorDetail: View {
                         .font(.largeTitle)
                     // current location
                     NavigationLink {
-                        Image(predator.image)
-                            .resizable()
-                            .scaledToFit()
+                        PredatorMap(position: .camera(MapCamera(
+                            centerCoordinate: predator.location,
+                            distance: 1000,
+                            heading: 250,
+                            pitch: 80
+                        )))
+                        .navigationTransition(.zoom(sourceID: 1, in: namespace))
                     } label: {
                         Map(position: $position) {
                             Annotation(predator.name, coordinate: predator.location) {
@@ -74,6 +80,7 @@ struct PredatorDetail: View {
                         }
                         .clipShape(.rect(cornerRadius: 15))
                     }
+                    .matchedTransitionSource(id: 1, in: namespace)
                     // appears in
                     Text("Appears In:")
                         .font(.title3)
